55807831886c5e4b13f0095560f7ef0e96e22757
Ramón Guardia López <ramon.guardialopez@gmail.com>
2018-01-18 20:29:57 +0100

    v 0.5 changed design.
    Added DB class.

---
+++ app/src/main/java/com/example/soul/pr9_shop/Product.java
@@ -0,0 +1,8 @@
+package com.example.soul.pr9_shop;
+
+/**
+ * Created by soul on 1/17/18.
+ */
+
+public class Product {
+}
---
+++ app/src/main/java/com/example/soul/pr9_shop/data/RepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.example.soul.pr9_shop.data;
+
+/**
+ * Created by soul on 1/18/18.
+ */
+
+public class RepositoryImpl {
+}
